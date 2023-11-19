c0300a42dbc02f48b8bee16c25d4c9ba6e527226
Dzejkop <jakubtrad@gmail.com>
2023-11-19 14:48:49 +0100

    Unflip the coordinate system

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,9 @@
 use crate::Vec3;
 
 pub fn zup2yup(Vec3 { x, y, z }: Vec3) -> Vec3 {
-    Vec3 { x: -x, y: z, z: y }
+    Vec3 { x: x, y: z, z: y }
 }
 
 pub fn yup2zup(Vec3 { x, y, z }: Vec3) -> Vec3 {
-    Vec3 { x: -x, y: z, z: y }
+    Vec3 { x: x, y: z, z: y }
 }
